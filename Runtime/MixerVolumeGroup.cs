ab777e110f651a97a4df023a752e9ac8e9d553cb
devolfer <devolfer05@gmail.com>
2024-09-11 15:46:16 +0200

    Added tooltips & documentation in mixer volume group

--- Runtime/MixerVolumeGroup.cs
+++ Runtime/MixerVolumeGroup.cs
@@ -5,26 +5,60 @@ using UnityEngine.Audio;
 
 namespace devolfer.Sound
 {
+    /// <summary>
+    /// Provides various functionality for handling the volume of an <see cref="AudioMixerGroup"/>.
+    /// </summary>
     [Serializable]
     public class MixerVolumeGroup
     {
+        [Tooltip("The Audio Mixer of the group.")]
         [SerializeField] private AudioMixer _audioMixer;
+
+        [Tooltip(
+            "The name of the exposed parameter of the group, e.g. 'VolumeMusic'" +
+            "\n\nYou will have to create an Audio Mixer Group and expose a parameter for it in the Editor yourself. " +
+            "Check any online-guides, if unsure how to do this.")]
         [SerializeField] private string _exposedParameter;
+
+        [Tooltip(
+            "The amount of segments the volume range of 0.0 - 1.0 will be split into. " +
+            "This will allow increasing/decreasing the volume in steps (e.g. 10 segments = 0.1 steps)." +
+            "\n\nSetting the volume to a specific value is of course still possible.")]
         [SerializeField, Min(1)] private int _volumeSegments;
 
+        /// <summary>
+        /// The name that exposes the Audio Mixer Groups' volume and allows manipulating it.
+        /// </summary>
         public string ExposedParameter => _exposedParameter;
 
+        /// <summary>
+        /// The current volume from 0.0 to 1.0.
+        /// </summary>
         public float VolumeCurrent => _volumeCurrent;
+        
+        /// <summary>
+        /// The current dB from -80 to 0.
+        /// </summary>
+        public float DecibelCurrent => _volumeCurrent != 0 ? Mathf.Log10(_volumeCurrent) * 20 : -80;
+        
+        /// <summary>
+        /// The amount of volume segments the range 0.0 to 1.0 is split into.
+        /// </summary>
         public int VolumeSegments => _volumeSegments;
+        
+        /// <summary>
+        /// Is the group muted?
+        /// </summary>
         public bool Muted => _muted;
 
         private float _volumeCurrent = 1;
         private float _volumePrevious = 1;
         private bool _muted;
 
-        internal MixerVolumeGroup(AudioMixer audioMixer,
-                                  string exposedParameter,
-                                  int volumeSegments)
+        /// <param name="audioMixer">The associated Audio Mixer of the group.</param>
+        /// <param name="exposedParameter">The name that exposes the Audio Mixer Groups' volume and allows manipulating it.</param>
+        /// <param name="volumeSegments">Splits the volume range into the specified amount of segments and allows setting volume in steps.</param>
+        public MixerVolumeGroup(AudioMixer audioMixer, string exposedParameter, int volumeSegments)
         {
             _audioMixer = audioMixer;
             _exposedParameter = exposedParameter;
@@ -44,12 +78,14 @@ namespace devolfer.Sound
 
             _volumePrevious = _volumeCurrent;
             _volumeCurrent = volume;
-            
+
             _muted = _volumeCurrent == 0;
         }
 
         internal void Increase()
         {
+            if (_volumeSegments <= 1) return;
+
             float segmentStep = 1f / _volumeSegments;
             float volumeCurrentRounded = Mathf.Round(_volumeCurrent / segmentStep) * segmentStep;
             Set(volumeCurrentRounded + segmentStep);
@@ -57,6 +93,8 @@ namespace devolfer.Sound
 
         internal void Decrease()
         {
+            if (_volumeSegments <= 1) return;
+
             float segmentStep = 1f / _volumeSegments;
             float volumeCurrentRounded = Mathf.Round(_volumeCurrent / segmentStep) * segmentStep;
             Set(volumeCurrentRounded - segmentStep);
@@ -75,7 +113,7 @@ namespace devolfer.Sound
         {
             return Fade(duration, targetVolume, EasingFunctions.GetEasingFunction(ease));
         }
-        
+
         internal IEnumerator Fade(float duration, float targetVolume, Func<float, float> easeFunction)
         {
             if (!_audioMixer.HasParameter(_exposedParameter))
@@ -84,7 +122,7 @@ namespace devolfer.Sound
                     $"Exposed Parameter {_exposedParameter} not found in {nameof(AudioMixer)} {_audioMixer}");
                 yield break;
             }
-            
+
             targetVolume = Mathf.Clamp01(targetVolume);
 
             if (duration <= 0)
@@ -124,7 +162,7 @@ namespace devolfer.Sound
 
     internal static class AudioMixerExtensions
     {
-        public static bool TrySetVolume(this AudioMixer mixer, string exposedParameter, ref float value)
+        internal static bool TrySetVolume(this AudioMixer mixer, string exposedParameter, ref float value)
         {
             value = Mathf.Clamp01(value);
             float decibel = value != 0 ? Mathf.Log10(value) * 20 : -80;
@@ -132,7 +170,7 @@ namespace devolfer.Sound
             return mixer.SetFloat(exposedParameter, decibel);
         }
 
-        public static bool TryGetVolume(this AudioMixer mixer, string exposedParameter, out float value)
+        internal static bool TryGetVolume(this AudioMixer mixer, string exposedParameter, out float value)
         {
             value = 0;
 
@@ -143,7 +181,7 @@ namespace devolfer.Sound
             return true;
         }
 
-        public static bool HasParameter(this AudioMixer mixer, string exposedParameter)
+        internal static bool HasParameter(this AudioMixer mixer, string exposedParameter)
         {
             return mixer.GetFloat(exposedParameter, out float _);
         }
