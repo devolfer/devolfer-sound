fab0b3826f0685943c76817546bae92657368a01
devolfer <devolfer05@gmail.com>
2024-09-11 08:28:00 +0200

    Added simple editor creation menu items

---
+++ Editor/MenuItems.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace devolfer.Sound
+{
+    public class MenuItems
+    {
+        [MenuItem("GameObject/Audio/Sound Manager", priority = 1, secondaryPriority = 0)]
+        private static void CreateSoundManager()
+        {
+            GameObject newGameObject = new("SoundManager", typeof(SoundManager));
+            Selection.activeGameObject = newGameObject;
+
+            SoundManager[] managers = Object.FindObjectsByType<SoundManager>(
+                FindObjectsInactive.Include,
+                FindObjectsSortMode.None);
+
+            if (managers.Length > 1)
+                Debug.Log($"There are more than one {nameof(SoundManager)} instances in the scene ({managers.Length})");
+        }
+        
+        [MenuItem("GameObject/Audio/Sound Emitter", priority = 1, secondaryPriority = 1)]
+        private static void CreateSoundEmitter()
+        {
+            GameObject newGameObject = new("SoundEmitter", typeof(SoundEmitter));
+            Selection.activeGameObject = newGameObject;
+        }
+    }
+}
