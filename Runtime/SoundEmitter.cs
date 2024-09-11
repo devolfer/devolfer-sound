ec9e783d6a003e2b290541d1b137910b0db05de4
devolfer <devolfer05@gmail.com>
2024-09-11 15:55:07 +0200

    Added sound emitter documentation

--- Runtime/SoundEmitter.cs
+++ Runtime/SoundEmitter.cs
@@ -5,11 +5,19 @@ namespace devolfer.Sound
     [RequireComponent(typeof(AudioSource))]
     public class SoundEmitter : MonoBehaviour
     {
+        /// <summary>
+        /// Is there an entity playing right now?
+        /// </summary>
+        public bool Playing => _entity != null && _entity.Playing;
+        
+        /// <summary>
+        /// Is there an entity paused right now?
+        /// </summary>
+        public bool Paused => _entity != null && _entity.Paused;
+
         private AudioSource _source;
         private SoundEntity _entity;
         private Transform _transform;
-        private bool Playing => _entity != null && _entity.Playing;
-        private bool Paused => _entity != null && _entity.Paused;
 
         private void Awake()
         {
@@ -18,6 +26,9 @@ namespace devolfer.Sound
             _transform = transform;
         }
 
+        /// <summary>
+        /// Plays attached AudioSource via the <see cref="SoundManager"/> as a <see cref="SoundEntity"/>.
+        /// </summary>
         public void Play()
         {
             if (Playing) return;
@@ -31,16 +42,25 @@ namespace devolfer.Sound
             _entity = SoundManager.Instance.Play(_source, _transform, onComplete: ClearEntity);
         }
 
+        /// <summary>
+        /// Pauses attached AudioSource via the <see cref="SoundManager"/> as a <see cref="SoundEntity"/>.
+        /// </summary>
         public void Pause()
         {
             if (Playing) SoundManager.Instance.Pause(_entity);
         }
 
+        /// <summary>
+        /// Resumes attached AudioSource via the <see cref="SoundManager"/> as a <see cref="SoundEntity"/>.
+        /// </summary>
         public void Resume()
         {
             if (Paused) SoundManager.Instance.Resume(_entity);
         }
 
+        /// <summary>
+        /// Stops attached AudioSource via the <see cref="SoundManager"/> as a <see cref="SoundEntity"/>.
+        /// </summary>
         public void Stop()
         {
             if (!Playing && !Paused) return;
@@ -50,15 +70,23 @@ namespace devolfer.Sound
             ClearEntity();
         }
 
+        /// <summary>
+        /// Fades attached AudioSource via the <see cref="SoundManager"/> for 2 seconds as a <see cref="SoundEntity"/>.
+        /// </summary>
+        /// <param name="targetVolume">The target volume the fade will reach at the end.</param>
         public void Fade(float targetVolume)
         {
             if (Playing) return;
 
             if (Paused) Resume();
-            
-            SoundManager.Instance.Fade(_entity, 2, targetVolume);
+
+            SoundManager.Instance.Fade(_entity, targetVolume, 2);
         }
 
+        /// <summary>
+        /// Fades in attached AudioSource via the <see cref="SoundManager"/> as a <see cref="SoundEntity"/>.
+        /// </summary>
+        /// <param name="duration">The duration in seconds the fade in will prolong.</param>
         public void FadeIn(float duration)
         {
             if (Playing || Paused) return;
@@ -70,7 +98,11 @@ namespace devolfer.Sound
                 fadeInDuration: duration,
                 onComplete: ClearEntity);
         }
-        
+
+        /// <summary>
+        /// Fades out attached AudioSource via the <see cref="SoundManager"/> as a <see cref="SoundEntity"/>.
+        /// </summary>
+        /// <param name="duration">The duration in seconds the fade out will prolong.</param>
         public void FadeOut(float duration)
         {
             if (!Playing && !Paused) return;
